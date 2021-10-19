58761d79ed68651b20b3bf0a308e0500cf7d93e5
perretpy <pierreyves.perret@gmail.com>
2021-10-19 20:57:35 +0200

    Add activities layouts

--- app/src/main/java/com/example/pintasso/ClassicActivity.java
+++ app/src/main/java/com/example/pintasso/ClassicActivity.java
@@ -9,5 +9,6 @@ public class ClassicActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic);
     }
 }

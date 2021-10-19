0d95165afb9eb770619d6fa54743400a2985ffc6
perretpy <pierreyves.perret@gmail.com>
2021-10-19 20:52:07 +0200

    Add activities

---
+++ app/src/main/java/com/example/pintasso/ClassicActivity.java
@@ -0,0 +1,13 @@
+package com.example.pintasso;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClassicActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/pintasso/PicassoActivity.java
@@ -0,0 +1,13 @@
+package com.example.pintasso;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PicassoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

5375e36b19d55b619606f2f167e9c03f1c24f58b
perretpy <pierreyves.perret@gmail.com>
2021-12-06 10:36:03 +0100

    Add classic activity

--- app/src/main/java/com/example/pintasso/ClassicActivity.java
+++ app/src/main/java/com/example/pintasso/ClassicActivity.java
@@ -1,14 +1,115 @@
 package com.example.pintasso;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pintasso.event.EventBusManager;
+import com.example.pintasso.event.SearchResultEvent;
+import com.example.pintasso.ui.ImageAdapterClassic;
+import com.squareup.otto.Subscribe;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ClassicActivity extends AppCompatActivity {
 
+    @BindView(R.id.recyclerView)
+    RecyclerView pRecyclerView;
+
+    @BindView(R.id.searchButton)
+    Button pSearchButton;
+
+    @BindView(R.id.searchText)
+    EditText pSearchText;
+
+    private ImageAdapterClassic pImageAdapter;
+
+    private int pNumberOfColumns = 3;
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classic);
+
+        // Binding ButterKnife annotations now that content view has been set
+        ButterKnife.bind(this);
+
+        pImageAdapter = new ImageAdapterClassic(this, new ArrayList<>());
+        pRecyclerView.setAdapter(pImageAdapter);
+        pRecyclerView.setLayoutManager(new GridLayoutManager(this, pNumberOfColumns));
+
+        pSearchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().trim().length()==0){
+                    pSearchButton.setEnabled(false);
+                } else {
+                    pSearchButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // While text is changing, hide list and show loader
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+    }
+
+    @OnClick(R.id.searchButton)
+    public void onButtonSearchClick() {
+        String query = this.pSearchText.getText().toString();
+        Log.d("SEARCH QUERY : ", query);
+        ImageSearchService.INSTANCE.searchImageFromQuery(query);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register to Event bus : now each time an event is posted, the activity will receive it if it is @Subscribed to this event
+        EventBusManager.BUS.register(this);
+
+        // Refresh search
+        //ImageSearchService.INSTANCE.searchImageFromQuery("test");
+        //Log.d("PICASSOACTIVITY", "Hello from here");
+    }
+
+    @Override
+    protected void onPause() {
+        // Unregister from Event bus : if event are posted now, the activity will not receive it
+        EventBusManager.BUS.unregister(this);
+
+        super.onPause();
+    }
+
+    @Subscribe
+    public void searchResult(final SearchResultEvent event) {
+        // Here someone has posted a SearchResultEvent
+        runOnUiThread (() -> {
+            // Step 1: Update adapter's model
+            pImageAdapter.setUrlList(event.getItemList());
+            pImageAdapter.notifyDataSetChanged();
+
+            // Step 2: hide loader
+            //TODO hide loader
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/pintasso/ui/ImageAdapterClassic.java
@@ -0,0 +1,105 @@
+package com.example.pintasso.ui;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pintasso.R;
+import com.example.pintasso.model.ImageItem;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ImageAdapterClassic extends RecyclerView.Adapter<ImageAdapterClassic.ViewHolder>{
+
+    private Activity context;
+    private LayoutInflater inflater;
+    private List<ImageItem> itemList;
+
+
+    public void setUrlList(List<ImageItem> itemList) {
+        this.itemList = itemList;
+    }
+
+
+    public ImageAdapterClassic(Activity context, List<ImageItem> itemList) {
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.image_item, parent, false);
+        ImageAdapterClassic.ViewHolder holder = new ImageAdapterClassic.ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("ImageAdapter", "Hello from onBindViewHolder" +itemList.toString());
+
+        // show The Image in a ImageView
+        new DownloadImageTask(holder.pImageView)
+                .execute(itemList.get(position).urls.thumb);
+
+
+        //Picasso.get().load(itemList.get(position).urls.thumb).into(holder.pImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.img_item)
+        ImageView pImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
+
